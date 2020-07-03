42bb1f9b0c04635bcad4ed83ea9d7b7a375433af
Shaji James <shajijames7@gmail.com>
2020-07-03 15:47:03 +0530

    Patching 1.0.2
    - Reduced build size by pulling sample datasets using web url request

--- build/lib/ctrl4ai/datasets.py
+++ build/lib/ctrl4ai/datasets.py
@@ -6,9 +6,9 @@ Created on Tue May 19 19:41:40 2020
 """
 
 import pandas as pd
+import requests
 pd.set_option('mode.chained_assignment', None)
 
-from . import helper
 
 def titanic():
   """
@@ -18,7 +18,9 @@ def titanic():
   Description: Sample Dataset
   Returns: Pandas DataFrame
   """
-  dataset=pd.read_csv(helper.__file__[:-len('helper')-3]+"sample_datasets/titanic.csv.gz",compression='gzip')
+  titanic_request = requests.get("https://github.com/vkreat-tech/ctrl4ai/raw/master/ctrl4ai/sample_datasets/titanic.csv.gz", allow_redirects=True)
+  open('titanic.csv', 'wb').write(titanic_request.content)
+  dataset=pd.read_csv('titanic.csv',compression='gzip')
   dataset=dataset.drop(['Unnamed: 0'],axis=1)
   return dataset
 
@@ -31,7 +33,9 @@ def trip_fare():
   Description: Sample Dataset
   Returns: Pandas DataFrame
   """
-  dataset = pd.read_csv(helper.__file__[:-len('helper')-3]+"sample_datasets/trip_fare.csv.gz",compression='gzip',parse_dates=['pickup_datetime','dropoff_datetime'])
+  trip_fare_request = requests.get("https://github.com/vkreat-tech/ctrl4ai/raw/master/ctrl4ai/sample_datasets/trip_fare.csv.gz", allow_redirects=True)
+  open('trip_fare.csv', 'wb').write(trip_fare_request.content)
+  dataset = pd.read_csv('trip_fare.csv',compression='gzip',parse_dates=['pickup_datetime','dropoff_datetime'])
   dataset=dataset.drop(['Unnamed: 0'],axis=1)
   return dataset
 
